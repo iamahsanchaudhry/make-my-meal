1115a611f2041157819d67eb3740aaebcfef9a94
Ahsan Javed <ahsanjavedd17@gmail.com>
2025-08-12 23:16:19 +0500

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Intro from "./components/Intro";
 import { MealForYou } from "./components/MealForYou";
-import { Outlet } from "@tanstack/react-router";
 
 export default function App() {
   return (
     <>
       <Intro />
       <MealForYou />
-      <Outlet />
-
-      
     </>
   );
 }
